b6b95ec337a6ce79a614982bf95d80f8d4d62569
Jorge Terence <jorge.terence@outlook.com>
2024-04-02 10:25:44 -0300

    Fixed TAP ex2

--- Técnicas Avançadas de Programação/ex2/src/Amigo.java
+++ Técnicas Avançadas de Programação/ex2/src/Amigo.java
@@ -1,26 +1,43 @@
 public class Amigo {
-	String nome;
-	int idade;
-	String telefone;
-	
-	static int populacao = 0;
-	
+	private String nome;
+	private int idade;
+	private String telefone;
+
 	public Amigo(String nome, int idade, String telefone) {
 		this.nome = nome;
 		this.idade = idade;
 		this.telefone = telefone;
-		Amigo.populacao++;
 	}
-	
+
 	public void aniversario() {
 		this.idade++;
 	}
-	
+
 	int idadeEmMeses() {
 		return this.idade * 12;
 	}
 
 	void setIdade(int idade) {
 		this.idade = idade;
+	}
+
+	int getIdade() {
+		return this.idade;
+	}
+
+	void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	String getNome() {
+		return this.nome;
+	}
+
+	void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	String getTelefone() {
+		return this.telefone;
 	}
 }
--- Técnicas Avançadas de Programação/ex2/src/App.java
+++ Técnicas Avançadas de Programação/ex2/src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Amigo amigo1 = new Amigo("João", 20, "1234-5678");
+        System.out.println("Idade em meses: " + amigo1.idadeEmMeses());
+        amigo1.aniversario();
+        System.out.println("Nova idade: " + amigo1.getIdade());
     }
 }
